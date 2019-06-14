a81ad88f02a266d275627edb45178e63bd9e4c4d
Anika <radianika@gmail.com>
2019-06-14 23:02:00 +0300

    Add component for todo items

--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,23 @@
 import React from 'react'
 
+
+import ToDoItem from './ToDoItem'
 class Main extends React.Component{
     constructor(){
         super()
-        this.state = {}
+        this.state = {
+            toDoItems: ['sleep', 'drink']
+        }
     }
     render(){
         return(
+            <div>
             <input  className="new-todo" type="text" placeholder="What needs to be done?" autoFocus />
+            <ul>
+                {this.state.toDoItems.map((item, index) => <ToDoItem key={index} value={item}/>)}
+
+            </ul>
+            </div>
         )   
     }
 }
