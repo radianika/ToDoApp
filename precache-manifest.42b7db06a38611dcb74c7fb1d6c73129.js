05d3806caecc87371ce7d3310060ead7e49f0808
Anika <radianika@gmail.com>
2019-06-14 21:49:51 +0300

    Updates

---
+++ precache-manifest.42b7db06a38611dcb74c7fb1d6c73129.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3cc0452723b28643fc952bb51ffa07e",
+    "url": "/ToDoApp/index.html"
+  },
+  {
+    "revision": "1d697ceb1630f0bfeab5",
+    "url": "/ToDoApp/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/ToDoApp/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "1d697ceb1630f0bfeab5",
+    "url": "/ToDoApp/static/js/main.921d9e17.chunk.js"
+  },
+  {
+    "revision": "fb1067834985d33aa2a1",
+    "url": "/ToDoApp/static/js/runtime~main.f2c45213.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ToDoApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
