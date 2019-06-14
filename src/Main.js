159ee357b205949024877b5984771afc90ad0664
Anika <radianika@gmail.com>
2019-06-14 23:40:02 +0300

    Add simple adding todo items

--- src/Main.js
+++ src/Main.js
@@ -6,13 +6,33 @@ class Main extends React.Component{
     constructor(){
         super()
         this.state = {
-            toDoItems: ['sleep', 'drink']
+            toDoItems: ['sleep', 'drink'],
+            placeholder: "What needs to be done?",
+        }
+        
+    }
+    addNewItem = (event) => {
+        if (event.keyCode === 13){
+            let newItem = event.target.value;
+            this.setState(prevstate => {
+                const newArray = [...prevstate.toDoItems];
+                newArray.push(newItem);
+                return(
+                    {
+                    toDoItems: newArray
+                    }
+                )
+            })
         }
     }
     render(){
         return(
             <div>
-            <input  className="new-todo" type="text" placeholder="What needs to be done?" autoFocus />
+            <input  className="new-todo" 
+                    type="text" 
+                    placeholder={this.state.placeholder} 
+                    autoFocus 
+                    onKeyDown={this.addNewItem} />
             <ul>
                 {this.state.toDoItems.map((item, index) => <ToDoItem key={index} value={item}/>)}
 
